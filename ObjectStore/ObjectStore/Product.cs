0e900790c0eb7c7e3de6e9ca8e4a8efd645246bd
martinnebesar <nebesar.martin@gmail.com>
2020-03-09 19:02:56 -0400

    Chapter21 uploaded

---
+++ ObjectStore/ObjectStore/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectStore
+{
+    [Serializable]
+    public class Product
+    {
+        public long Id;
+        public string Name;
+        public double Price;
+
+        [NonSerialized]
+        string Notes;
+
+        public Product(long id, string name, double price, string notes)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Notes = notes;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}: {1} (${2:F2}) {3}", Id, Name, Price, Notes);
+        }
+    }
+}
